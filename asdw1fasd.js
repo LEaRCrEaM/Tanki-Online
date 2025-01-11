36f7408b9f24f0095aebfc2705c3b9b645865319
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-01-11 16:07:25 -0500

    Update asdw1fasd.js

--- asdw1fasd.js
+++ asdw1fasd.js
@@ -43,7 +43,7 @@
                 /*setTimeout(() => {
                     User.patata = '';
                 }, 5000);*/
-                if ((User.name.length > 2) && ([/*'Midway', 'Queen.of.Spain', 'Arpecu9_KunuT_Bo_MHe', 'DerMar'*/"Scars"].includes(User.name))) {
+                if ((User.name.length > 2) && ([/*'Midway', 'Queen.of.Spain', 'Arpecu9_KunuT_Bo_MHe', 'DerMar'*/].includes(User.name))) {
                     alert(`Access denied`);
                     for (const k in window) {
                         window[k] = 't';
