6c434015ebfac2e4f38d9c4e29330265b5e4d861
LEaRCrEaM <127627494+LEaRCrEaM@users.noreply.github.com>
2025-09-27 10:31:41 -0400

    Update fasfas2.js

--- fasfas2.js
+++ fasfas2.js
@@ -1299,7 +1299,7 @@ Element.prototype.appendChild = function() {
                 flagPos2 = null;
                 myTankIntPos = null;
             };
-        } else if (Array.from(this.classList).length == 0) {
+        } else if (Array.from(this.classList).length == 1) {
             if (!isGameActive) {
                 isGameActive = true;
                 console.log('joined game');
